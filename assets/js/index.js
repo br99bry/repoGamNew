f93d9295c71d5a65be05c384ddac8f2ac60fceab
Bryan <bryanblack1999@gmail.com>
2021-04-06 21:09:53 -0500

    version preliminar enfermedades section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 let buttonReadMore = document.querySelectorAll('.leerMas-btn');
 for(let i=0; i<buttonReadMore.length;i++){
   buttonReadMore[i].addEventListener('click', () => {
-    if(buttonReadMore[i].textContent=='▲'){
-      buttonReadMore[i].innerHTML='▼';
+    if(buttonReadMore[i].textContent=='Leer más'){
+      buttonReadMore[i].innerHTML='Leer menos';
     }else{
-      buttonReadMore[i].innerHTML='▲';
+      buttonReadMore[i].innerHTML='Leer más';
     }
   })
 }
